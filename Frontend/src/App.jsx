0415dcbf21c75feb3ba9b7f674cf7e4549f1d41c
Sarthak <sarthakbhandari49@gmail.com>
2025-05-12 09:11:49 +0530

    Add boiler plate

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Editor } from "./components/organisms/Editor/Editor";
+
+function App() {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+}
+
+export default App;
---
+++ backend/src/config/serverConfig.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// eslint-disable-next-line no-undef
+export const PORT = process.env.PORT || 3000;
